c6266fa694661cf69239d92ee511ef0813619021
Tim Paine <3105306+timkpaine@users.noreply.github.com>
2026-08-28 10:40:31 -0400

    Bump version: 0.2.5 → 0.2.6
    Signed-off-by: Tim Paine <3105306+timkpaine@users.noreply.github.com>

--- spaday_regular_layout/__init__.py
+++ spaday_regular_layout/__init__.py
@@ -4,7 +4,7 @@ from spaday import ComponentPackage
 
 from .components import RegularLayoutFrame, SpadayRegularLayout
 
-__version__ = "0.2.5"
+__version__ = "0.2.6"
 
 package = ComponentPackage(
     name="regular-layout",
